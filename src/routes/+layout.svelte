f7ab96b6ee4c6d9452f5a391e7105721fadc9517
Levi Bucsis <levibucsis@gmail.com>
2025-10-09 23:54:51 -0700

    Add resetLevelCounter to demo layout
    Integrate navigation fix into demo app for proper title cascading during client-side navigation.

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,12 @@
 <script>
-	import { Title } from '../lib/index.js'
+	import { page } from '$app/state'
+	import { Title, resetLevelCounter } from '../lib/index.js'
+
+	// Reset level counter on route changes for consistent hierarchy
+	$effect(() => {
+		page.url.pathname
+		resetLevelCounter()
+	})
 </script>
 
 <Title title="Svelte Title Demo" level={0} separator=" 🔸 " />
